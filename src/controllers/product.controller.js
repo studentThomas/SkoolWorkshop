87a3f4e0694a2a840cc5c472bfcb1fb4c2867e3c
studentThomas <twa.vermeulen@student.avans.nl>
2023-05-25 11:00:38 +0200

    Added getProduct route

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,38 @@
+const logger = require("../util/logger").logger;
+const pool = require("../util/mysql-db");
+
+const productController = {
+  getProducts: (req, res, next) => {
+    let sqlStatement = "SELECT * FROM `product`";
+
+    pool.getConnection(function (err, conn) {
+      if (err) {
+        next({
+          status: 409,
+          message: err.message,
+        });
+      }
+
+      conn.query(sqlStatement, function (err, results, fields) {
+        if (err) {
+          next({
+            status: 409,
+            message: err.message,
+          });
+        }
+        const products = results;
+
+        if (results) {
+          res.status(200).json({
+            status: 200,
+            message: "All products are retrieved",
+            data: products,
+          });
+        }
+        pool.releaseConnection(conn);
+      });
+    });
+  },
+};
+
+module.exports = productController;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const productController = require("../controllers/product.controller");
 
 router.use(express.json());
+
+router.get("/product", productController.getProducts);
 
 module.exports = router;
